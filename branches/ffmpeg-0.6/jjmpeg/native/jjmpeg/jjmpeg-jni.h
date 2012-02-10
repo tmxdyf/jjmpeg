7bded32c4375b944370be5d6906767ee415cc898
notzed@gmail.com <notzed@gmail.com@0ea11c16-f98b-17d3-1077-06809ec25a26>
2012-02-10 21:40:53 +0000

    Verified against ffmpeg 0.6

---
+++ branches/ffmpeg-0.6/jjmpeg/native/jjmpeg/jjmpeg-jni.h
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2011 Michael Zucchi
+ *
+ * This file is part of jjmpeg, a java binding to ffmpeg's libraries.
+ *
+ * jjmpeg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * jjmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with jjmpeg.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+ * Global includes
+ */
+
+#include <jni.h>
+
+// If any more of these added: time to move it to a per-arch file
+
+#if defined(WIN64) || defined(WIN32)
+#  include <windows.h>
+
+#  define _TOSTR(x) #x
+#  define TOSTR(x) _TOSTR(x)
+
+#  define DLOPEN(x, lib, ver) do { x = LoadLibrary(lib "-"  _TOSTR(ver) ".dll"); if (x == NULL) { fprintf(stderr, "cannot open %s\n",  lib "-" TOSTR(ver) ".dll"); fflush(stderr); return 0; } } while(0)
+#  define CALLDL(x) (*d ## x)
+#  define MAPDL(x, lib) do { if ((d ## x = (void *)GetProcAddress(lib, #x)) == NULL) { fprintf(stderr, "cannot resolve %s\n", #x); fflush(stderr); return 0; } } while(0)
+#else
+#  include <dlfcn.h>
+
+#  if (defined (__APPLE__) && defined (__MACH__))
+#    define SO ".dylib"
+#  else
+#    define SO ".so"
+#  endif
+
+#  define DLOPEN(x, lib, ver) x = dlopen("lib" lib SO, RTLD_LAZY|RTLD_GLOBAL); do { if (x == NULL) { fprintf(stderr, "cannot open %s\n", lib ".so"); fflush(stderr); return 0; } } while (0)
+#  define CALLDL(x) (*d ## x)
+#  define MAPDL(x, lib) do { if ((d ## x = dlsym(lib, #x)) == NULL) { fprintf(stderr, "cannot resolve %s\n", #x); fflush(stderr); return 0; } } while (0)
+#endif
+
+#define ADDR(jp) (jp != NULL ? (*env)->GetDirectBufferAddress(env, jp) : NULL)
+#define SIZE(jp) (jp != NULL ? (*env)->GetDirectBufferCapacity(env, jp) : 0)
+#define STR(jp) (jp != NULL ? (*env)->GetStringUTFChars(env, jp, NULL) : NULL)
+#define RSTR(jp, cp) ((jp != NULL) ? ((*env)->ReleaseStringUTFChars(env, jp, cp)):0 )
+
+#define WRAP(cp, clen) ((*env)->NewDirectByteBuffer(env, cp, clen))
+#define WRAPSTR(js) ((*env)->NewStringUTF(env, js))
+
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavutil/avutil.h>
+
+static int init_local(JNIEnv *env);
+
+// swscale leaves SwsContext opaque.  We just need a dummy type for the binding library.
+#include <libswscale/swscale.h>
+struct SwsContext {
+	int dummy;
+};
+typedef struct SwsContext SwsContext;
+
+// same for ReSampleContext
+struct ReSampleContext {
+	int dummy;
+};
+//typedef struct ReSampleContext ReSampleContext;
+
+
+/**  Library handles */
+static void *avutil_lib;
+static void *avcodec_lib;
+static void *avformat_lib;
+static void *swscale_lib;
