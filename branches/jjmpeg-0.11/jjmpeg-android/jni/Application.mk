e4a40426ffc5060debd7855e9d2849effcf4ebd6
notzed <notzed@0ea11c16-f98b-17d3-1077-06809ec25a26>
2012-06-23 07:39:36 +0000

    More build script frobnication. Builds each android target one at a time now, based on a configure script. makedist generates multiple arch packages.

--- branches/jjmpeg-0.11/jjmpeg-android/jni/Application.mk
+++
@@ -1,4 +0,0 @@
-
-# armeabi-v7a crashes on my tablet, so fuck that
-APP_ABI := armeabi
-#APP_ABI := armeabi
--- branches/jjmpeg-0.11/jjmpeg-core/config.mk
+++ branches/jjmpeg-0.11/jjmpeg-core/config.mk
@@ -1,4 +1,12 @@
-VERSION=0.11.1a1
+# Release version
+VERSION=0.11.1a2
+# Java SDK location
 JAVA_SDK=/usr/java/latest
+# For android release, key store location
 KEYSTORE=/home/notzed/.ssh/jjmpeg-release-key.keystore
+# Android SDK
 ANDROID_SDK=/usr/local/android-sdk-linux
+# Android NDK
+ANDROID_NDK=/usr/local/android-ndk-r7c
+# Which binary targets to build for android
+ANDROID_TARGETS=armeabi armeabi-v7a
