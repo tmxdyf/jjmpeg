1755b6675656922c75c125c3cc868ef23b827c91
notzed <notzed@0ea11c16-f98b-17d3-1077-06809ec25a26>
2012-06-25 08:28:11 +0000

    Some more build fiddling.

--- jjmpeg-core/config.mk
+++ jjmpeg-core/config.mk
@@ -1,5 +1,10 @@
-# Release version
-VERSION=0.11.1a2
+# Which binary targets to build for android
+ANDROID_TARGETS=armeabi armeabi-v7a
+# Java cross-compilation targets
+JAVA_CROSS_TARGETS=mswin-amd64
+# Java native-compilation targets
+JAVA_HOST_TARGETS=gnu-amd64 gnu-i386
+
 # Java SDK location
 JAVA_SDK=/usr/java/latest
 # For android release, key store location
@@ -8,5 +13,8 @@ KEYSTORE=/home/notzed/.ssh/jjmpeg-release-key.keystore
 ANDROID_SDK=/usr/local/android-sdk-linux
 # Android NDK
 ANDROID_NDK=/usr/local/android-ndk-r7c
-# Which binary targets to build for android
-ANDROID_TARGETS=armeabi armeabi-v7a
+
+# Release version name
+VERSION=0.11.1a2
+# Library version tag
+LIBVERSION=011
